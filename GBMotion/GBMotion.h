996eab5e1f44b599e0d932650c17396142e973a6
Luka Mirosevic <luka@goonbee.com>
2013-09-09 13:45:13 +0200

    Initial Commit

---
+++ GBMotion/GBMotion.h
@@ -0,0 +1,13 @@
+//
+//  GBMotion.h
+//  GBMotion
+//
+//  Created by Luka Mirosevic on 09/09/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBMotion : NSObject
+
+@end
---
+++ GBMotion/GBMotion.m
@@ -0,0 +1,13 @@
+//
+//  GBMotion.m
+//  GBMotion
+//
+//  Created by Luka Mirosevic on 09/09/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBMotion.h"
+
+@implementation GBMotion
+
+@end
